07f5f377c68929538f81f4e7d02a2f709f2f60eb
Chánh Phạm <phamchanh6599@gmail.com>
2020-11-09 10:57:23 +0700

    Add helpers

---
+++ src/helpers/CoreApi/CorePagination.js
@@ -0,0 +1,12 @@
+const fetchList = async (url) => {
+    try {
+        const res = await fetch(url)
+        return await res.json()
+    } catch (err) {
+        throw new Error(`Fetch fail: ${err}`)
+    }
+}
+
+export {
+    fetchList
+}
