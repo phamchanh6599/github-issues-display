82e9230b5cb85e4bb475743d6210e0d6ef7f8ba9
Chánh Phạm <phamchanh6599@gmail.com>
2020-11-09 10:58:39 +0700

    Add common components

---
+++ src/components/View/View.js
@@ -0,0 +1,48 @@
+import React, {useEffect, useState} from 'react';
+
+import './View.scss';
+import Table from './../../core-components/Table/Table'
+import Pagination from './../../core-components/Pagination/Pagination'
+
+import * as corePagination from './../../helpers/CoreApi/CorePagination'
+
+function View() {
+    const [list, setList] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [issuesPerPage] = useState(5);
+
+    useEffect(() => {
+        const fetch = async () => {
+            try {
+                setLoading(true);
+                const data = await corePagination.fetchList(`https://api.github.com/repos/rails/rails/issues?page=${currentPage}&per_page=5`);
+                if (data.length) {
+                    console.log("data", data)
+                    setList(data)
+                }
+            } catch(err) {
+                console.error("ERR", err)
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetch()
+    }, [currentPage])
+
+    // Change page
+    const paginate = pageNumber => setCurrentPage(pageNumber)
+
+    return (
+        <div className="View">
+            <Table listIssues={list} loading={loading} />
+            <Pagination 
+                paginate={paginate}
+                currentPage={currentPage}
+                loading={loading}
+            />
+        </div>
+    );
+}
+
+export default View;
