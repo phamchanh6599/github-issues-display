7d87b19b2cc250309aa1c1deb513555d30b5a1aa
Chánh Phạm <phamchanh6599@gmail.com>
2020-11-09 11:00:43 +0700

    Clean img, extra files and install package node-sass

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import React from 'react';
+
+import './App.scss';
+import View from './components/View/View' 
 
 function App() {
   return (
     <div className="App">
-        S
+        <View />
     </div>
   );
 }
